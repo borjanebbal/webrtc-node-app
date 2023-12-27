70f373a00bc7ab4bc18a5ea04aa2f3127aba925a
Borja Nebbal <borja.nebbal@gmail.com>
2023-12-27 18:09:31 +0100

    chore: upgrade dependencies

--- server.js
+++ server.js
@@ -1,21 +1,23 @@
 const express = require('express')
+const http = require('http')
+const { Server } = require('socket.io')
+
 const app = express()
-const server = require('http').createServer(app)
-const io = require('socket.io')(server)
+const server = http.createServer(app)
+const io = new Server(server)
 
 app.use('/', express.static('public'))
 
 io.on('connection', (socket) => {
   socket.on('join', (roomId) => {
-    const selectedRoom = io.sockets.adapter.rooms[roomId]
-    const numberOfClients = selectedRoom ? selectedRoom.length : 0
+    const selectedRoom = io.sockets.adapter.rooms.get(roomId)
+    const numberOfClients = selectedRoom ? selectedRoom.size : 0
 
-    // These events are emitted only to the sender socket.
-    if (numberOfClients == 0) {
+    if (numberOfClients === 0) {
       console.log(`Creating room ${roomId} and emitting room_created socket event`)
       socket.join(roomId)
       socket.emit('room_created', roomId)
-    } else if (numberOfClients == 1) {
+    } else if (numberOfClients === 1) {
       console.log(`Joining room ${roomId} and emitting room_joined socket event`)
       socket.join(roomId)
       socket.emit('room_joined', roomId)
@@ -28,19 +30,22 @@ io.on('connection', (socket) => {
   // These events are emitted to all the sockets connected to the same room except the sender.
   socket.on('start_call', (roomId) => {
     console.log(`Broadcasting start_call event to peers in room ${roomId}`)
-    socket.broadcast.to(roomId).emit('start_call')
+    socket.to(roomId).emit('start_call')
   })
+
   socket.on('webrtc_offer', (event) => {
     console.log(`Broadcasting webrtc_offer event to peers in room ${event.roomId}`)
-    socket.broadcast.to(event.roomId).emit('webrtc_offer', event.sdp)
+    socket.to(event.roomId).emit('webrtc_offer', event.sdp)
   })
+
   socket.on('webrtc_answer', (event) => {
     console.log(`Broadcasting webrtc_answer event to peers in room ${event.roomId}`)
-    socket.broadcast.to(event.roomId).emit('webrtc_answer', event.sdp)
+    socket.to(event.roomId).emit('webrtc_answer', event.sdp)
   })
+
   socket.on('webrtc_ice_candidate', (event) => {
     console.log(`Broadcasting webrtc_ice_candidate event to peers in room ${event.roomId}`)
-    socket.broadcast.to(event.roomId).emit('webrtc_ice_candidate', event)
+    socket.to(event.roomId).emit('webrtc_ice_candidate', event)
   })
 })
 
